c32e5e2d0ce5dae889957d781af6ceebd2b219c9
icyMojito <dev.icymojito@gmail.com>
2020-06-05 04:38:05 +0900

    format test 3

---
+++ src/test/java/wooteco/subway/doc/Documentation.java
@@ -0,0 +1,17 @@
+package wooteco.subway.doc;
+
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.restassured3.RestDocumentationFilter;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class Documentation {
+	public static RestDocumentationFilter create(String identifier) {
+		return document("favorite/" + identifier,
+		                requestFields(
+		                		fieldWithPath("source").type(JsonFieldType.STRING).description("you")
+		                ));
+	}
+}
